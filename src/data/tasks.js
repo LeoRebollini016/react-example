d0cc5a686d2b8a8955b8c111f6a30c925ca1ce9e
LeoRebollini <leorebollini@gmail.com>
2024-07-08 03:41:15 -0300

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+    {
+        id: 0,
+        title: 'first tasks',
+        description: "my first tasks",
+    },
+    {
+        id: 1,
+        title: 'Second tasks',
+        description: "my second tasks",
+    },
+    {
+        id: 2,
+        title: 'Third tasks',
+        description: "my third tasks",
+    },
+]
